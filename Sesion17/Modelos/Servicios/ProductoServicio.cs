fe3d28e6b213a5a3ed78b1cb8a09b79a0b47d24a
cfbj11 <cfbriones@uamv.edu.ni>
2024-10-14 08:12:38 -0600

    Se agrego Formularios

---
+++ Sesion17/Modelos/Servicios/ProductoServicio.cs
@@ -0,0 +1,38 @@
+﻿using Sesion17.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion17.Servicios
+{
+    internal class ProductoServicio
+    {
+        List<Producto> productos = new List<Producto>();
+
+        public void AgregarProducto(Producto producto)
+        {
+            productos.Add(producto);
+        }
+
+        public Producto BuscarProducto(Producto producto)
+        {
+            return productos.Find(prod => prod.ID == producto.ID);
+        }
+
+        public void EliminarProducto(Producto producto)
+        {
+            productos.Remove(producto);
+        }
+
+        public void ActualizarProducto(Producto producto, int id)
+        {
+            int index = productos.FindIndex(prod => prod.ID == id);
+            if (index != -1)
+            {
+                productos[index] = producto;
+            }
+        }
+    }
+}
